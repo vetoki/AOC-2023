22115307b5b6e73c37739f84de677ed25dbe1ba3
Anton Classon <anton@kumpan.se>
2023-12-01 12:46:35 +0100

    Init

---
+++ Day1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2023
+{
+    public static class Day1
+    {
+        public static void RunDay()
+        {
+            Console.WriteLine("Day 1");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AOC_2023;
+
+Console.WriteLine("Hello, World!");
+
+
+Day1.RunDay();
